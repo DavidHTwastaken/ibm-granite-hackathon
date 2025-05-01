b9ac5de2f8c544ecc594421b2d5345051077abd9
Heisn Nithysingha <heisn.nithy@gmail.com>
2025-05-01 04:07:47 -0400

    Implemened Barebones Frontend
    - Connected Flask and React
    - Set up router to have many pages
    - implemented audio recorder to record and upload audio to the server
    - implemented uploading zip file into server
    - implemented Transcript box which can be altered in  back or front end (both are synched)
    - added Generate Placeholder button to be used later
    - removes temp files when server starts for ease
    - Set up multiple components for main.js
    - added very low level navbar to switch to any other page, so far only placeholder about us

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import {HashRouter as Router, Route, Routes} from "react-router-dom";
-import { Main } from "./Pages/Main";
-import { About } from "./Pages/About";
+import Main from "./Pages/Main";
+import About from "./Pages/About";
+import { Navbar } from "./Components/Navbar";
+
 function App() {
 
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Main/>} />
         <Route path="/about" element={<About/>} />
