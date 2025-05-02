fe506737aa4f47a9a6be6d76a77ca7b78e956b7e
DavidHTwastaken <david.houletymeczko@ontariotechu.net>
2025-05-01 21:03:58 -0400

    Local attempt at instruct model

--- models/doc.py
+++ models/doc.py
@@ -1,16 +1,23 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import time
 
-def generate_doc():
+def generate_doc(project):
+    start_load_models = time.time()
     device = "cpu"  # or "cpu"
-    model_path = "ibm-granite/granite-8b-code-instruct-4k"
+    # "ibm-granite/granite-8b-code-instruct-4k"
+    model_path = 'ibm-granite/granite-3b-code-instruct-2k'
     tokenizer = AutoTokenizer.from_pretrained(model_path)
     # drop device_map if running on CPU
-    model = AutoModelForCausalLM.from_pretrained(model_path, device_map=device)
+    model = AutoModelForCausalLM.from_pretrained(model_path)
     model.eval()
+    time_load_models = time.time() - start_load_models
+    start_gen = time.time()
     # change input text as desired
     chat = [
-        {"role": "user", "content": ""},
+        {"role": "user", 
+        "content": f'Create a markdown document explaining the following project. \
+            I will provide the file paths followed by their content. Here is the project:\n {project}'},
     ]
     chat = tokenizer.apply_chat_template(
         chat, tokenize=False, add_generation_prompt=True)
@@ -24,5 +31,19 @@ def generate_doc():
     # decode output tokens into text
     output = tokenizer.batch_decode(output)
     # loop over the batch to print, in this example the batch size is 1
+    time_gen = time.time() - start_gen
+    print(f"Load models time: {time_load_models:.2f}s")
+    print(f"Generation time: {time_gen:.2f}s")
+    return output
+
+def main():
+    start = time.time()
+    project = open('tmp.txt', 'r').read()
+    load_time = time.time() - start
+    output = generate_doc(project)
+    print(f"Load project time: {load_time:.2f}s")
     for i in output:
         print(i)
+
+if __name__ == '__main__':
+    main()
